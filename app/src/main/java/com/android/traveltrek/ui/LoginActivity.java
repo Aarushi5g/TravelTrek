b9e70e4d3b84e849c361b35cdc5aaba5c5efdf96
Priyanshu <priyanshu.khandelwal.19cs@bmu.edu.in>
2021-09-20 16:58:43 +0530

    Added Login signup screen

---
+++ app/src/main/java/com/android/traveltrek/ui/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.android.traveltrek.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.traveltrek.MainActivity;
+import com.android.traveltrek.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        TextView newregister = findViewById(R.id.textViewsignup);
+        Button btn1 = findViewById(R.id.signin);
+        newregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/android/traveltrek/ui/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.android.traveltrek.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.traveltrek.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+        TextView btn = findViewById(R.id.alreadytext);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
