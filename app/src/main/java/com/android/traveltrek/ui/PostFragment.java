16803183033b7aff07792df9d6b7d6d3f24e85c9
Aarushi Gupta <aarushigupta55555@gmail.com>
2021-10-08 01:15:34 +0530

    Added the post fragment ui and made changes in login & signup ui

--- app/src/main/java/com/android/traveltrek/ui/PostFragment.java
+++ app/src/main/java/com/android/traveltrek/ui/PostFragment.java
@@ -1,35 +1,47 @@
 package com.android.traveltrek.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.android.traveltrek.R;
 import com.android.traveltrek.databinding.FragmentPostBinding;
 
 public class PostFragment extends Fragment {
 
-    private FragmentPostBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentPostBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
 
-        return root;
+        String [] photo_categories = {"","Nature","Portrait","City life","Old times","Beauty"};
+        Spinner spinner = view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, photo_categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        CardView cardView = view.findViewById(R.id.pickerCard);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivity(Intent.createChooser(intent, "Select a File to Upload"));
     }
 }
