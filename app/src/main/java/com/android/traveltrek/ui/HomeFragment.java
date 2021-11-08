c2ce1dfdd9850f13f6ac7141fc417186e373e632
Aarushi Gupta <aarushigupta55555@gmail.com>
2021-11-08 13:32:07 +0530

    error resolve

--- app/src/main/java/com/android/traveltrek/ui/HomeFragment.java
+++ app/src/main/java/com/android/traveltrek/ui/HomeFragment.java
@@ -222,7 +222,7 @@ public class HomeFragment extends Fragment {
 
         View marker = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_marker, null);
 
-        CircleImageView markerImage = (CircleImageView) marker.findViewById(R.id.user_dp);
+        CircleImageView markerImage = (CircleImageView) marker.findViewById(R.id.profileimage);
         markerImage.setImageResource(resource);
         TextView txt_name = (TextView)marker.findViewById(R.id.name);
         txt_name.setText(_name);
