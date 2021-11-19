18be127e1d902737d07b1ee78fae1afb391406d1
Aarushi Gupta <aarushigupta55555@gmail.com>
2021-11-19 20:27:01 +0530

    Implement image clicker for posting

--- app/src/main/java/com/android/traveltrek/ui/PostFragment.java
+++ app/src/main/java/com/android/traveltrek/ui/PostFragment.java
@@ -1,11 +1,17 @@
 package com.android.traveltrek.ui;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -15,13 +21,19 @@ import androidx.fragment.app.Fragment;
 import com.android.traveltrek.R;
 import com.android.traveltrek.databinding.FragmentPostBinding;
 
+import java.io.ByteArrayOutputStream;
+
 public class PostFragment extends Fragment {
+
+    ImageView imageView;
+    private static int REQUEST_CODE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_post, container, false);
 
+        imageView = view.findViewById(R.id.image3);
         String [] photo_categories = {"","Nature","Portrait","City life","Old times","Beauty"};
         Spinner spinner = view.findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, photo_categories);
@@ -32,17 +44,35 @@ public class PostFragment extends Fragment {
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,
+                        REQUEST_CODE);
 
-                showFileChooser();
             }
         });
 
         return view;
     }
 
-    private void showFileChooser() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivity(Intent.createChooser(intent, "Select a File to Upload"));
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+                        byteArray.length);
+
+                imageView.setImageBitmap(bitmap);
+
+                Uri uri = data.getData();
+
+            }
+        }
     }
 }
